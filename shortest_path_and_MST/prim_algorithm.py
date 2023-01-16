ac534ac1e282ec872a16452a6c4151db1281fd84
Kiril Lazarov <kirillazarov@abv.bg>
2023-01-17 00:22:06 +0200

    Task.

---
+++ shortest_path_and_MST/prim_algorithm.py
@@ -0,0 +1,67 @@
+from queue import PriorityQueue
+
+
+class Edge:
+    def __init__(self, first, second, weight):
+        self.first = first
+        self.second = second
+        self.weight = weight
+
+    def __gt__(self, other):
+        return self.weight > other.weight
+
+
+def prim(node, graph, forest, forest_edges):
+    forest.add(node)
+
+    pk = PriorityQueue()
+    for edge in graph[node]:
+        pk.put(edge)
+
+    while not pk.empty():
+        min_edge = pk.get()
+        non_tree_node = -1
+
+        if min_edge.first in forest and min_edge.second not in forest:
+            non_tree_node = min_edge.second
+        elif min_edge.second in forest and min_edge.first not in forest:
+            non_tree_node = min_edge.first
+
+        if non_tree_node == -1:
+            continue
+
+        forest.add(non_tree_node)
+        forest_edges.append(min_edge)
+
+        for edge in graph[non_tree_node]:
+            pk.put(edge)
+
+
+
+edges = int(input())
+
+graph = {}
+
+for _ in range(edges):
+    first, second, weight = [int(x) for x in input().split(', ')]
+    if first not in graph:
+        graph[first] = []
+
+    if second not in graph:
+        graph[second] = []
+
+    edge = Edge(first, second, weight)
+    graph[first].append(edge)
+    graph[second].append(edge)
+
+forest = set()
+
+forest_edges = []
+
+for node in graph:
+    if node in forest:
+        continue
+    prim(node, graph, forest, forest_edges)
+
+for edges in forest_edges:
+    print(f'{edges.first} - {edges.second}')
